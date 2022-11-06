73f19912919ef91adefd3d27c0999e0c982a4bee
francesperez <frances.perez141@gmail.com>
2022-11-06 12:12:00 -0600

    Basic statements lecture notes

---
+++ basic_statements_lecture.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Cocteau Twins', 'The Moon and the Melodies', 1986, 0.00, 'Ambient, Dream Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Cocteau Twins', 'Treasure', 1984,.090, 'Ethereal Wave, Dream Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Katy Perry', 'One of the Boys', 2008, 7.0, 'Pop Rock');
