13d9627e424cd779f0b0abfd127b9cd8397bf562
francesperez <frances.perez141@gmail.com>
2022-11-07 16:10:56 -0600

    Slight changes after lecture

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,9 +1,11 @@
-UPDATE albums SET sales = sales * 10;
+USE codeup_test_db;
+
 SELECT artist, name, sales FROM albums;
+UPDATE albums SET sales = sales * 10;
 
 
-UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
 SELECT artist, name, release_date FROM albums WHERE release_date < 1980;
+UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
 
-UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
 SELECT * FROM albums WHERE artist='Peter Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
