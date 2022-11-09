4fd4a23bb738e95f1a73a2ff28076150bdc3bb52
francesperez <frances.perez141@gmail.com>
2022-11-09 10:36:07 -0600

    refactored some code, shortened a few lines. Added 'AS' to change column names

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,36 +1,32 @@
 USE employees;
 
 # Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
-SELECT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
-      SELECT CONCAT(first_name,' ',last_name) FROM employees
+      SELECT CONCAT(first_name,' ',last_name) AS 'full_name' FROM employees
       WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
       AND first_name LIKE 'E%' AND first_name LIKE '%E';
 
 # Find all employees born on Christmas — 842 rows.
-SELECT * FROM employees
-      WHERE YEAR(birth_date) BETWEEN 1930 AND 2000
-      AND MONTH(birth_date) = 12
+SELECT * FROM employees WHERE
+      MONTH(birth_date) = 12
       AND DAY(birth_date) = 25;
 
 # Find all employees hired in the 90s and born on Christmas — 362 rows.
 SELECT * FROM employees
-      WHERE YEAR(birth_date) BETWEEN 1930 AND 2000
-      AND MONTH(birth_date) = 12
+      WHERE MONTH(birth_date) = 12
       AND DAY(birth_date) = 25
       AND YEAR(hire_date) BETWEEN 1990 AND 1999;
 
 # Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
 SELECT * FROM employees
-      WHERE YEAR(birth_date) BETWEEN 1930 AND 2000
-      AND MONTH(birth_date) = 12
+      WHERE MONTH(birth_date) = 12
       AND DAY(birth_date) = 25
       AND YEAR(hire_date) BETWEEN 1990 AND 1999
-      ORDER BY YEAR(birth_date), YEAR(hire_date) DESC;
+      ORDER BY birth_date, hire_date DESC;
 
 # For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have
 # been working at the company
-SELECT *, DATEDIFF(NOW(), hire_date) FROM employees
-      WHERE YEAR(birth_date) BETWEEN 1930 AND 2000
-      AND MONTH(birth_date) = 12
+SELECT *, DATEDIFF(NOW(), hire_date) AS days_working FROM employees
+      WHERE MONTH(birth_date) = 12
       AND DAY(birth_date) = 25
-      AND YEAR(hire_date) BETWEEN 1990 AND 1999;
+      AND YEAR(hire_date) LIKE '199%'
+      ORDER BY days_working DESC;
